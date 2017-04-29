48dbd3b3b83efe808487cbdcb385e77c34a3c16b
Jens Struwe <jens@struwe.net>
2017-04-29 18:19:04 +0000

    add capistrano deployment config

---
+++ config/deploy.rb
@@ -0,0 +1,113 @@
+# config valid only for current version of Capistrano
+lock '3.6.1'
+
+# set :application, 'my_app_name'
+# set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'afeefa-frontend-frontend-api'
+set :repo_url, 'https://github.com/AfeefaDe/afeefa-frontend-api.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/afeefa/rails/afeefa-frontend-frontend-api'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets'#, 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :env, 'production'
+set :rails_env, fetch(:env)
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# Defaults to nil (no asset cleanup is performed)
+# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
+# set this to the number of versions to keep
+set :keep_assets, 2
+
+namespace :deploy do
+
+  task :restart do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        api =
+            if fetch(:stage).to_s == 'production'
+              'frontend-api'
+            else
+              'dev-frontend-api'
+            end
+        execute "svc -du ~/service/#{api}" # maybe we can use -h instead of -du
+      end
+    end
+  end
+
+  task :stop do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        api =
+            if fetch(:stage).to_s == 'production'
+              'frontend-api'
+            else
+              'dev-frontend-api'
+            end
+        execute "svc -d ~/service/#{api}" # maybe we can use -h instead of -du
+      end
+    end
+  end
+
+  task :start do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      within release_path do
+        api =
+            if fetch(:stage).to_s == 'production'
+              'frontend-api'
+            else
+              'dev-frontend-api'
+            end
+        execute "svc -u ~/service/#{api}" # maybe we can use -h instead of -du
+      end
+    end
+  end
+
+  task :seed_db do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        if fetch(:stage).to_s == 'production'
+          execute 'echo seed job is skipped for stage production'
+        else
+          execute "cd #{release_path} && RAILS_ENV=production bundle exec rake db:seed"
+        end
+      end
+    end
+  end
+
+end
+
+after 'deploy', 'deploy:seed_db'
+after 'deploy', 'deploy:restart'
+after 'deploy:rollback', 'deploy:restart'
