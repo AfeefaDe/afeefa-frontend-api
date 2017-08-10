7cc501539e3aee04231413db7cf832203fe3b8de
Jens Struwe <jens@struwe.net>
2017-08-10 18:33:48 +0200

    enable marketentries area query parameter

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,15 @@ class EntriesController < ApplicationController
   SUPPORTED_LOCALES = %w(ar en es fa fr ku ps ru sq sr ti tr ur)
 
   def index
+    area = if params['area'].present? && ['leipzig', 'bautzen'].include?(params['area']) then params['area'] else 'dresden' end
+
     if !params['locale'].present? || (params['locale'].present? && params['locale'] == DEFAULT_LOCALE)
-      orgas = get_entries(Orga)
-      events = get_entries(Event)
+      orgas = get_entries(Orga, area)
+      events = get_entries(Event, area)
     else
       if params['locale'].in?(SUPPORTED_LOCALES)
-        orgas = get_entries(Orga, with_translations: true)
-        events = get_entries(Event, with_translations: true)
+        orgas = get_entries(Orga, area, with_translations: true)
+        events = get_entries(Event, area, with_translations: true)
       else
         raise 'locale is not supported'
       end
@@ -27,11 +29,12 @@ class EntriesController < ApplicationController
 
   private
 
-  def get_entries(klazz, with_translations: false)
+  def get_entries(klazz, area, with_translations: false)
     entries =
         klazz.
             includes(:category, :sub_category, :locations, :contact_infos, :parent_orga, parent_orga: :contact_infos).
-            where(state: 'active')
+            where(state: 'active').
+            where(area: area)
     if with_translations
       entries = entries.includes(:translation_caches)
     end
