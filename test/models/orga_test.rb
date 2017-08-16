b3d4c7f7e59448f3fa13c1fd19586d76b0e59125
Steve Reinke <steve_is@gmx.de>
2017-08-16 14:09:44 +0200

    [FEATURE] add test for tag in json, refs #169

---
+++ test/models/orga_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class OrgaTest < ActiveSupport::TestCase
+
+  test 'should have tags' do
+    tags = 'tag1,tag2'
+    assert orga = Orga.create(tags: tags)
+    json = JSON.parse(orga.to_json)
+    assert_equal tags, json['tags']
+  end
+
+end
