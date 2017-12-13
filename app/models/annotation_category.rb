86dcca728fd3658ed91307db0284e7918f32ee1b
Steve Reinke <steve_is@gmx.de>
2017-12-13 17:16:51 +0100

    [TASK] rename annotation category for external entries and remove detail, refs #396

--- app/models/annotation_category.rb
+++ app/models/annotation_category.rb
@@ -15,7 +15,7 @@ class AnnotationCategory < ApplicationRecord
     end
 
     def external_entry
-      @@entry ||= AnnotationCategory.where(title: 'Externe Eintragung').last.freeze
+      @@entry ||= AnnotationCategory.where(title: 'EXTERNE EINTRAGUNG').last.freeze
     end
   end
 
