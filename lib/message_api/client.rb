81a3073e6b46ffa7b9124f76877b150d1d25a652
Steve Reinke <steve_is@gmx.de>
2018-03-14 16:35:17 +0100

    [FEATURE] new configs for message api, refs #434

--- lib/message_api/client.rb
+++ lib/message_api/client.rb
@@ -4,20 +4,14 @@ module MessageApi
   module Client
     class << self
       def notify_for_new_entry(model:)
-        params = {
+        payload = {
+          key: api_key,
+          area: model.area || 'dresden',
+
           title: model.title,
           description: model.short_description,
           backend_link: backend_link(model)
-        }
-
-        if contact_data = get_contact_data(model)
-          params.reverse_merge!(contact_data)
-        end
-
-        payload = {
-          key: api_key,
-          area: model.area || 'dresden'
-        }.merge(params)
+        }.merge(get_contact_data(model) || {})
 
         http_client.send_new_entry_info(payload)
       end
@@ -34,7 +28,10 @@ module MessageApi
 
         payload = {
           key: api_key,
-          area: model.area || 'dresden'
+          area: model.area || 'dresden',
+
+          title: model.title,
+          frontend_link: frontend_link(model)
         }.merge(params.except(:type, :id))
 
         http_client.send_entry_contact_message(payload)
@@ -45,7 +42,11 @@ module MessageApi
 
         payload = {
           key: api_key,
-          area: model.area || 'dresden'
+          area: model.area || 'dresden',
+
+          title: model.title,
+          backend_link: backend_link(model),
+          frontend_link: frontend_link(model)
         }.merge(params.except(:type, :id))
 
         http_client.send_entry_feedback_info(payload)
@@ -62,7 +63,27 @@ module MessageApi
       end
 
       def backend_link(model)
-        "#{Settings.backend_api.path}/#{model.class.name.underscore.pluralize}/#{model.id}/edit"
+        type =
+          case model.class
+          when Orga
+            'orgas'
+          when Event
+            'events'
+          end
+        path = Settings.backend_api.path || 'https://backend.afeefa.de'
+        "#{path}/#{type}/#{model.id}"
+      end
+
+      def frontend_link(model)
+        type =
+          case model.class
+          when Orga
+            'project'
+          when Event
+            'event'
+          end
+        path = Settings.frontend_ui.path || 'https://afeefa.de'
+        "#{path}/#{type}/#{model.id}"
       end
 
       def get_contact_data(model)
@@ -98,7 +119,7 @@ module MessageApi
       end
 
       def send_entry_feedback_info(payload)
-        HTTP.post("#{base_path}/send/messageFromUserToOwner",
+        HTTP.post("#{base_path}/send/feedbackFromUserToAdmins",
           headers: { 'Content-Type' => 'application/json' },
           body: payload.to_json)
       end
